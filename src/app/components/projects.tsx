1b74ffadfe1e9336f598022368683938e465faef
Amith Jayapraban <j.amith.amith@gmail.com>
2026-03-24 19:53:05 +0530

    refactor: update component imports to use absolute paths and replace SVG icon with ArrowRight component

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ArrowRight } from "lucide-react";
 
 function Projects() {
   return (
@@ -11,21 +12,7 @@ function Projects() {
       <h3 className="mb-3  flex items-center gap-1 text-xl font-medium  ">
         Projects
         <span className="inline-block  transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-arrow-right"
-          >
-            <path d="M5 12h14" />
-            <path d="m12 5 7 7-7 7" />
-          </svg>
+          <ArrowRight size={24} />
         </span>
       </h3>
       <p className="m-0   break-words text-sm text-neutral-600">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Head } from "./components/head";
+import { Head } from "@/components/head";
 import React from "react";
-import Skills from "./components/skills";
-import Socials from "./components/socials";
-import Experience from "./components/experience";
-import Projects from "./components/projects";
+import Skills from "@/components/skills";
+import Socials from "@/components/socials";
+import Experience from "@/components/experience";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
